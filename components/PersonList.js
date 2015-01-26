6d24c2bab1490778c97b49ffad839eea57210a26
Simon Guest <simon.guest@concur.com>
2015-01-26 09:52:37 -0800

    Added return for no matches

--- components/PersonList.js
+++ components/PersonList.js
@@ -5,6 +5,7 @@ var PersonList = React.createClass({
         return <PersonPanel key={person.email} name={person.name} email={person.email} avatar={person.avatar}/>
       });
     }
+    if (!people) return <h1>No matching people found</h1>;
     return (
       <div>
       {people}
