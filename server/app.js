2f2b2dbb5b7de13cf8ff26ddc4887b3a3f11c7b1
Simon Guest <simon.guest@concur.com>
2015-02-03 15:25:11 -0800

    Tidied up app.js

--- server/app.js
+++ server/app.js
@@ -1,19 +1,21 @@
 var express = require('express');
 var app = express();
 
-require('node-jsx').install({extension:'.jsx'});
+require('node-jsx').install({
+	extension: '.jsx'
+});
 
 var port = process.env.PORT || 8000;
 
-app.set('views', __dirname+'/views');
+app.set('views', __dirname + '/views');
 
-app.use('/scripts', express.static(__dirname+'/scripts'));
+app.use('/scripts', express.static(__dirname + '/scripts'));
 
-app.use('/', function(req, res){
-  res.render('index.ejs')
-  //ReactAsync.renderToStringAsync(App(),function(err, markup) {
-  //  res.send('<!DOCTYPE html>'+markup);
-  //});
+app.use('/', function(req, res) {
+	res.render('index.ejs')
+		//ReactAsync.renderToStringAsync(App(),function(err, markup) {
+		//  res.send('<!DOCTYPE html>'+markup);
+		//});
 });
 
 console.log('Server running on port %d', port);
